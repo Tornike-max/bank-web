2ea39cbf680b757210f2e9cb5df4b0adfaf33ce9
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2023-04-07 16:38:20 +0400

    slider

--- script.js
+++ script.js
@@ -175,11 +175,45 @@ tabsContainer.addEventListener('click',function(e){
     //add active content
     document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
 
-})
+});
 
-//slider
 
-let slide = document.querySelectorAll('.slide')
+
+const slides = document.querySelectorAll('.slide')
+let btnLeft = document.querySelector('.slider__btn--left')
+let btnRight = document.querySelector('.slider__btn--right')
+let curSlide = 0;
+let maxSlide = slides.length;
+
+
+
+let goToSlide = function(slide){
+    slides.forEach((s,i)=> (s.style.transform = `translateX(${100 * (i - slide)}%)`))
+}
+goToSlide(0)
+
+const nextSlide = function(){
+    if(curSlide === maxSlide - 1){
+        curSlide = 0;
+    }else{
+        curSlide++;
+    }
+    goToSlide(curSlide); 
+}
+
+const prevSlide = function(){
+    if(curSlide === 0){
+        curSlide = maxSlide - 1
+    }else{
+        curSlide--;
+    }
+    goToSlide(curSlide);
+}
+
+btnRight.addEventListener('click',nextSlide)
+btnLeft.addEventListener('click',prevSlide)
+
+
 
 
 
